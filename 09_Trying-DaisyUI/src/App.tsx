143def0a30d0f750d1dbbdef33627c41d62db3d1
AmithBV0606 <amithrao0606@gmail.com>
2024-11-08 07:57:07 +0530

    Studied about Daisy UI and it's usage

---
+++ 09_Trying-DaisyUI/src/App.tsx
@@ -0,0 +1,14 @@
+import Card from "./components/Card";
+// import Carousal from "./components/Carousal";
+
+export default function App() {
+  return (
+    <div className="h-screen flex justify-center items-center bg-slate-300">
+      <Card />
+      {/* <Carousal /> */}
+    </div>
+    // <div>
+    //   <Carousal />
+    // </div>
+  )
+}
